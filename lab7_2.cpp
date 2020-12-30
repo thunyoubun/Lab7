4c7d565300a1b39d74b72aa196983adab4daa05c
Switch <thunyoubun@gmail.com>
2020-12-31 00:22:46 +0700

    Lab7

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(float score)
+{
+	char grade;
+
+	grade = (score > 90) ? 'A' : (score > 75) ? 'B' : (score > 60) ? 'C' : (score > 45) ? 'D' : 'F';
+    
+	return grade;
+}
+
+
+
 
 int main(){
 	//Input the number of students
@@ -18,8 +29,11 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
+
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
@@ -29,6 +43,7 @@ int main(){
 	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
 	cout << "---------------------------------------------\n";
 	while(i < N){
+		
 		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
 		i++;
 	} 
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -9,3 +9,51 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int age,height,pro;
+    cout << "Enter your age: ";
+    cin >> age;
+        if(age<=20){
+            
+            cout << "Enter your height: ";
+            cin >> height;
+            if(height<160){
+                cout << "Your status = UNFRIEND";
+            }else if(height < 175){
+                cout << "Your status = FRIEND";
+            }else
+            {
+                cout <<  "Enter your property: ";
+                cin >> pro;
+                    if(pro > 69000000){
+                        cout << "Your status = MARRIED";
+                    }else{
+                        cout << "Your status = ONE-NIGHT-STAND";
+                    }
+            }
+        
+         }else if(age<30){
+            cout <<  "Enter your property: ";
+            cin >> pro;
+            if(pro>10000000){
+                cout << "Your status = BEST FRIEND";
+            }else
+            {
+                cout << "Your status = UNFRIEND";
+            }
+            
+        }else{
+           cout << "Your status = UNFRIEND";
+        }
+        
+
+
+
+
+    return 0;
+}
